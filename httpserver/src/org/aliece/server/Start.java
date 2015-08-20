ea5602f2472c03e8e40b78f79f5ccc10c0680b03
Aliece <alieceyy@gmail.com>
2015-08-20 15:33:28 +0800

    1

--- httpserver/src/org/aliece/server/Start.java
+++ httpserver/src/org/aliece/server/Start.java
@@ -4,16 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class Start {
-	public static void main(String[] args) throws IOException {
-		args = new String[2];
-		args[0]="12345";
-		args[1]="F://";
-		int port = Integer.parseInt(args[0]);
-		final Server s = new Server(port, new ServerEventHandler(args[1], 4));
-		System.out.println("Starting server on port " + port);
-		System.out.println("Web root folder: "
-				+ new File(args[1]).getAbsolutePath());
-		while (true)
-			s.listen();
-	}
+    public static void main(String[] args) throws IOException {
+        args = new String[2];
+        args[0] = "12345";
+        args[1] = "F://";
+        int port = Integer.parseInt(args[0]);
+        final Server s = new Server(port, new ServerEventHandler(args[1], 4));
+        System.out.println("Starting server on port " + port);
+        System.out.println("Web root folder: "
+                + new File(args[1]).getAbsolutePath());
+        while (true)
+            s.listen();
+    }
 }
